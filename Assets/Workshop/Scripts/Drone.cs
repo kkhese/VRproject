0274e828b0c1fb9cd2667ef27657da9a7f1a732c
kkhese <100791121+kkhese@users.noreply.github.com>
2023-04-18 18:20:06 -0600

    workshop add1

---
+++ Assets/Workshop/Scripts/Drone.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Needs AI Nav Mesh Agent
+[RequireComponent(typeof(UnityEngine.AI.NavMeshAgent))]
+public class Drone : MonoBehaviour {
+	UnityEngine.AI.NavMeshAgent agent;
+	// Tower location
+	Transform tower;
+	// Attack frequency
+	public float ATTACK_TIME = 2;
+	float attackTime = 0;
+	// Maximum HP for the character
+	public int MAX_HP = 3;
+	[System.NonSerialized]
+	// initialize variable hp
+	public int hp = 0;
+
+	// initialize attack distance to 1.
+	public float ATTACK_DISTANCE = 1;
+	// Use this for initialization
+	void Start () {
+		// find nav mesh agent component
+		agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+		// Set target to the object tagged 'Tower'
+		tower = GameObject.Find("Tower").transform;
+		// Go to the target
+		agent.destination = tower.position;
+		// Initial hp is Max
+		hp = MAX_HP;
+		// attacking freqency set as default
+		attackTime = ATTACK_TIME;
+	}
+
+	void Update()
+	{
+		// If enemy is close to the target less than attack distance, start attack
+		if(agent.remainingDistance <= ATTACK_DISTANCE)
+		{
+			attackTime += Time.deltaTime;
+			if(attackTime > ATTACK_TIME)
+			{
+				attackTime = 0;
+				// Add damage to Tower
+				Tower.Instance.Damage();
+			}
+		}
+	}
+}
---
+++ Assets/Workshop/Scripts/Tower.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Tower : MonoBehaviour
+{
+
+	public static Tower Instance;
+
+	// Slider
+	public Slider hpSlider;
+	public int MAX_HP = 100;
+	int hp = 0;
+	// Die and Mission Success
+	public GameObject die;
+	public GameObject succeed;
+
+	void Awake()
+	{
+		if (Instance == null)
+			Instance = this;
+	}
+	// Start with Max HP
+	void Start()
+	{
+		hp = MAX_HP;
+	}
+	// If hp becomes less than or equal with 0, the player die
+	public void Damage()
+	{
+		hp = hp - 1;
+		hpSlider.value = hp;
+		if (hp <= 0)
+		{
+			if (die)
+			{
+				die.SetActive(true);
+			}
+		}
+	}
+	// Somehow hp drops whenever enemy is spawned
+	public void Plus()
+	{
+		hp++;
+	}
+	// 'K-pop star' come and collide with right hand, game over
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.CompareTag("star"))
+		{
+			succeed.SetActive(true);
+		}
+	}
+}
